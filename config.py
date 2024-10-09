09accad5b76870602f59d57cd9f913bfa0a8d5eb
NTF-KURAMA <aveline.teamx@gmail.com>
2024-10-09 19:33:44 +0600

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ import os
 # Required Variables Config
 API_ID = int(os.environ.get("API_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", ""))
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7769509314:AAEIxnQ34ja5KfJYEBF3S-GbBU18Eof-fmI")
+ADMIN = int(os.environ.get("ADMIN", "7219138626"))
 
 
 # Premium 4GB Renaming Client Config
@@ -15,11 +15,11 @@ STRING_SESSION = os.environ.get("STRING_SESSION", "")
 
 # Log & Force Channel Config
 FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002368029778"))
 
 
 # Mongo DB Database Config
-DATABASE_URL = os.environ.get("DATABASE_URL", "")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Kurana:kurama@cluster0.dxbzy.mongodb.net/")
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "CodeXBots")
 
 
